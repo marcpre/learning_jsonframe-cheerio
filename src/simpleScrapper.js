92ca847d75af4e856c1cb5918367a11e83394429
marcpre <marcus.presich@gmail.com>
2017-10-29 09:18:51 +0100

    simple scrapper

---
+++ src/simpleScrapper.js
@@ -0,0 +1,17 @@
+const cheerio = require('cheerio')
+const jsonframe = require('jsonframe-cheerio')
+
+const $ = cheerio.load(`
+<body>
+  <h1>I love jsonframe!</h1>
+  <span itemprop="email"> Email: gabin@datascraper.pro  </span>
+<body>`)
+
+jsonframe($) // initializing the plugin
+
+let frame = {
+	"title": "h1", // this is an inline selector
+	"email": "span[itemprop=email] < email" // output an extracted email
+}
+
+console.log( $('body').scrape(frame, { string: true } ))
